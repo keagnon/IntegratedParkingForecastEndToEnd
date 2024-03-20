f610d00f6b7a2e8a1200ae3369af06868a982874
keagnon <keagnon.gbe@gmail.com>
2024-03-20 19:32:40 +0100

    Adding function to load data in aszur storage

--- ETL/azure_configuration.py
+++ ETL/azure_configuration.py
@@ -3,23 +3,23 @@ from io import StringIO
 
 class AzureConfiguration:
 
-	def __init__(self, azure_storage_connection_string, container_name, blob_name):
-    	self.azure_storage_connection_string = azure_storage_connection_string
-    	self.container_name = container_name
-    	self.blob_name = blob_name
-    	self.blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
+    def __init__(self, azure_storage_connection_string, container_name, blob_name):
+        self.azure_storage_connection_string = azure_storage_connection_string
+        self.container_name = container_name
+        self.blob_name = blob_name
+        self.blob_service_client = BlobServiceClient.from_connection_string(azure_storage_connection_string)
 
-	def push_dataframe_to_blob(self, dataframe):
-    	file_name = f"{self.blob_name}.csv"
+    def push_dataframe_to_blob(self, dataframe):
+        file_name = f"{self.blob_name}.csv"
 
-    	# Convertir le DataFrame au format CSV
-    	csv_data = dataframe.to_csv(index=False)
+        # Convertir le DataFrame au format CSV
+        csv_data = dataframe.to_csv(index=False)
 
-    	# Convertir les données CSV en bytes
-    	data_bytes = csv_data.encode('utf-8')
+        # Convertir les données CSV en bytes
+        data_bytes = csv_data.encode('utf-8')
 
-    	# Envoyer les données au blob
-    	blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
-    	blob_client.upload_blob(data_bytes, overwrite=True)
+        # Envoyer les données au blob
+        blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file_name)
+        blob_client.upload_blob(data_bytes, overwrite=True)
 
-    	print(f"Dataframe envoyé avec succès à Azure Blob Storage : {file_name}")
+        print(f"Dataframe envoyé avec succès à Azure Blob Storage : {file_name}")
--- ETL/velib.py
+++ ETL/velib.py
@@ -10,72 +10,72 @@ from azure_configuration import AzureConfiguration
 from azure.storage.blob import BlobServiceClient
 
 
-class extractor():
+class Extractor:
+    """Goal : Extract data from velib API"""
 
-	""" Goal : Extract data from velib API"""
+    def __init__(self, url):
+        self.url = url
 
-	def __init__(self,url):
-   	    self.url=url
-
-	def extract_data(self):
-        """ Extract data from API"""
-    	response=requests.get(self.url)
-    	data=response.json()
+    def extract_data(self):
+        """Extract data from API"""
+        response = requests.get(self.url)
+        data = response.json()
         return data
 
-class transformer():
 
-	""" Goal : Transform data extracted"""
+class Transformer:
+    """Goal : Transform data extracted"""
 
-	def __init__(self,extractor):
-    	self.extractor=extractor
+    def __init__(self, extractor):
+        self.extractor = extractor
 
-	def create_dataframe(self):
-    	""" Create dataframe base on data collected """
-    	stations_info=self.extractor.extract_data()
-    	stations_info=stations_info["data"]["stations"]
-    	df=pd.DataFrame(stations_info)
-    	return df
+    def create_dataframe(self):
+        """Create dataframe base on data collected """
+        stations_info = self.extractor.extract_data()
+        stations_info = stations_info["data"]["stations"]
+        df = pd.DataFrame(stations_info)
+        return df
 
-if __name__=='__main__':
 
-	urls = [
-    	("station_info", "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"),
-    	("station_status", "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json")
-	]
+if __name__ == '__main__':
 
-	list_dfs=[]
+    urls = [
+        ("station_info", "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_information.json"),
+        ("station_status", "https://velib-metropole-opendata.smovengo.cloud/opendata/Velib_Metropole/station_status.json")
+    ]
 
-	for name, url in urls:
-    	# Data extraction
-    	extractor_instance = extractor(url)
-    	data = extractor_instance.extract_data()
-    	#print(data)
+    list_dfs = []
 
-    	# Data transformation
-    	transformer_instance = transformer(extractor_instance)
-    	df = transformer_instance.create_dataframe()
-    	print(df)
+    for name, url in urls:
+        # Data extraction
+        extractor_instance = Extractor(url)
+        data = extractor_instance.extract_data()
+        # print(data)
 
-    	list_dfs.append(df)
+        # Data transformation
+        transformer_instance = Transformer(extractor_instance)
+        df = transformer_instance.create_dataframe()
+        print(df)
 
-	# Dataframe concatanation using station_id as join key
+        list_dfs.append(df)
 
-	concatenated_df=pd.concat(list_dfs,ignore_index=True)
-	print(concatenated_df)
+    # Dataframe concatenation using station_id as join key
+
+    concatenated_df = pd.concat(list_dfs, ignore_index=True)
+    print(concatenated_df)
 
     config = configparser.ConfigParser()
-    config.read('config.ini')
+    config.read('configuration.ini')
     azure_storage_connection_string = config['Azure']['storage_connection_string']
     container_name = config['Azure']['container_name']
     blob_name = config['Azure']['blob_name']
 
-	# Create an instance of azure_config
-	azure_config = AzureConfiguration(
-    	azure_storage_connection_string=azure_storage_connection_string,
-    	container_name=container_name,
-    	blob_name=blob_name
-	)
+    # Create an instance of azure_config
+    azure_config = AzureConfiguration(
+        azure_storage_connection_string=azure_storage_connection_string,
+        container_name=container_name,
+        blob_name=blob_name
+    )
 
-	# Push dataframe concata
-	azure_config.push_dataframe_to_blob(concatenated_df)
+    # Push dataframe concatenation
+    azure_config.push_dataframe_to_blob(concatenated_df)
